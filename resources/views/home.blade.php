ea5f377f6e5b859f6c9f2458ec67c30b4b0d84a1
khinchanny <johnny@2ntkh.com>
2017-02-08 16:13:00 +0700

    add auth as form login and register

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default well-sm well-lg well clearfix">
+                <div class="col-md-2">Sidebar</div>
+                <div class="col-md-10">Content</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
