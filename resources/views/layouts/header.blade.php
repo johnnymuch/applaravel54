098c2c0c0176f8f9dddab3d923cd9bf5da43be63
khinchanny <johnny@2ntkh.com>
2017-02-10 16:13:20 +0700

    function login

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	{!! Html::style('css/bower_cononents.bootstrap.bootstrap.min.js') !!}
+	
+</head>
+<body>
+
+	
+</body>
+</html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,15 @@
+<div class="container">
+	<div class="row">
+		<div class="panel-body">Login Area</div>
+		<div class="col-md-4 col-md-offset-4">
+			<div class="from-group">
+				<label for="email">Email</div>
+			<input class="form-control" type="text" name="email" id="email" placeholder="Please Enter Name Hare">
+			</div>
+		<div class="form-group">
+			<label for="password">Password</label>
+			<input class="form-control" name="password" id="password" placeholder="Please Enter Your Password">
+		</div>
+	</div>		
+</div>    
+
